0c6de5b966cf71e6449c702187227870c596d6de
Ilya Sysoi <ilya.sysoi@gmail.com>
2019-11-22 23:02:38 +0300

    added PostgreSQL and updated WordCategory model

--- Sources/App/Models/WordCategory.swift
+++ Sources/App/Models/WordCategory.swift
@@ -6,27 +6,36 @@
 //
 
 import Foundation
+import FluentPostgreSQL
 import Vapor
 
-struct WordCategory {
-    let id: Int
-    let name: String
-    let imageURL: String
+struct WordCategory: PostgreSQLModel {
     
-    init(id: Int, name: String, imageURL: String) {
+    static let entity: String = "category"
+    static let name = "category"
+    
+    var id: Int?
+    var name: String
+    var image: String
+    
+    init(id: Int? = nil, name: String, image: String) {
         self.id = id
         self.name = name
-        self.imageURL = imageURL
+        self.image = image
     }
 }
 
+/// Allows `WordCategory` to be used as a dynamic migration.
+extension WordCategory: PostgreSQLMigration { }
+
 struct UserWordCategory: Content {
-    let id: Int
+    
+    let id: Int?
     let name: String
     let imageURL: String
     let isSelected: Bool
     
-    init(id: Int, name: String, imageURL: String, isSelected: Bool) {
+    init(id: Int?, name: String, imageURL: String, isSelected: Bool) {
         self.id = id
         self.name = name
         self.imageURL = imageURL
@@ -36,21 +45,8 @@ struct UserWordCategory: Content {
     init(category: WordCategory, isSelected: Bool) {
         self.init(id: category.id,
                   name: category.name,
-                  imageURL: category.imageURL,
+                  imageURL: "images/categories/\(category.image)",
                   isSelected: isSelected)
     }
     
 }
-
-let categoriesExample = [UserWordCategory(id: 1,
-                                          name: "Common words",
-                                          imageURL: "/images/categories/common_words.jpeg",
-                                          isSelected: true),
-                         UserWordCategory(id: 2,
-                                          name: "Food",
-                                          imageURL: "/images/categories/food.jpeg",
-                                          isSelected: false),
-                         UserWordCategory(id: 3,
-                                          name: "Family",
-                                          imageURL: "/images/categories/family.jpeg",
-                                          isSelected: false)]
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,6 +1,9 @@
 import Vapor
 
 private let apiVersion = "api/v1"
+struct PostgreSQLVersion: Codable {
+    let version: String
+}
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
@@ -9,8 +12,22 @@ public func routes(_ router: Router) throws {
         return "It works!"
     }
     
+    router.get("sql") { req in
+        return req.withPooledConnection(to: .psql) { conn in
+            return conn.raw("SELECT version()")
+                .all(decoding: PostgreSQLVersion.self)
+        }.map { rows in
+            return rows[0].version
+        }
+    }
+    
     router.get("\(apiVersion)/categories") { req in
-        return categoriesExample
+        return WordCategory.query(on: req).all().map { (wordCategories) -> [UserWordCategory] in
+            return wordCategories.map {
+                return UserWordCategory.init(category: $0,
+                                             isSelected: true)
+            }
+        }
     }
     
     router.get("\(apiVersion)/words") { req in
